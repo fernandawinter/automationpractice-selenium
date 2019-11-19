118c4ced04dc98e864ef8d365dc98868491fa811
Fernanda Winter <fernanda.winter@hotmail.com>
2019-11-18 23:44:05 -0300

    Finalização do teste

--- README.md
+++ README.md
@@ -8,12 +8,11 @@ Ter o Google Chrome instalado no computador, bem como o node.js.
 
 ## Como rodar a aplicação
 
-Basta digitar no terminal `npm install` e em seguida `npm test`.
-Caso apareça o erro `Error: Could not find update-config.json. Run 'webdriver-manager update' to download binaries` será necessário realizar o comando `npx webdriver-manager update`.
+- Basta digitar no terminal `npm install` (onde o npm irá ler o que está contido no arquivo package.json as dependências listadas, fazendo com o que o teste funcione). Em seguida, digitar o comando `npx webdriver-manager update`. Por fim, digitar `npm test`(que é o próprio teste).
 
 ## Como foi construído
 
-- Javascript puro foi utilizado no código, e também Protector. O framework Selenium foi utilizado.
+- Javascript puro foi utilizado no código, e tambémo framework Selenium. Protractor também foi utilizando, o que permite que se use Selenium utilizando a linguagem Javascript. 
 
 ## Como consegui realizar a tarefa proposta
 Nunca havia utilizado Selenium para realizar testes, mas gostei da ferramenta. Por não conhecer tanto assim, recorri a alguns links da Internet para ir montando o exercício.
@@ -21,12 +20,14 @@ Nunca havia utilizado Selenium para realizar testes, mas gostei da ferramenta. P
 Segue o link do vídeo que mostra o teste funcionando: [https://youtu.be/buju965bwX4](https://youtu.be/buju965bwX4)
 
 ## O que eu poderia ter feito se tivesse mais tempo
+- Realizar um teste para o passo 9 (Valide o valor total da compra)
 - Fazer testes menores pra testar de forma independente cada etapa do site 
 - Fazer os testes utilizando Java + Selenium em um outro repositório
 
-
 ## Referências
 Links de onde encontrei material para estudar/entender tópicos que ainda não havia entendido muito bem do Javascript utilizando Selenium:
+
+* [https://www.youtube.com/watch?v=7bzbcyfli50](https://www.youtube.com/watch?v=7bzbcyfli50)
 
 * [https://techblog.poppulo.com/removing-sleeps-from-your-angularjs-protractor-tests/](https://techblog.poppulo.com/removing-sleeps-from-your-angularjs-protractor-tests/)
 
--- src/specs/automation-practice.spec.js
+++ src/specs/automation-practice.spec.js
@@ -5,113 +5,84 @@ const USER_DATA = require('../data/user-data')
 
 describe("Automation Practice", function () {
     browser.ignoreSynchronization = true; // This allows to protractor to run on regular website, not specific to angular 
-   
+
     it("Sucessful purchase of a product", function () {
-        //1.          Acessar o site: www.automationpractice.com. 
-
-        //Visitar a URL www.automationpractice.com ('')
-
+        //1. Acessar o site: www.automationpractice.com. 
         const URL = 'http://automationpractice.com/index.php?'
 
         browser.get(URL);
-        
-        //2.          Escolha um produto qualquer na loja.
-        
-        //Clicar no nome do primeiro produto da home page 
+
+        //2. Escolha um produto qualquer na loja.
         helper.waitForElement(SELECTORS.HOME_PAGE_PRODUCT_LINK);
         element(by.css(SELECTORS.HOME_PAGE_PRODUCT_LINK)).click();
-        
+
+        //3. Adicione o produto escolhido ao carrinho.
         helper.waitForElement(SELECTORS.ADD_TO_CART_BUTTON);
-
-        //3.          Adicione o produto escolhido ao carrinho.
-        //Clicar no botão "Add to cart"
         element(by.css(SELECTORS.ADD_TO_CART_BUTTON)).click();
+
+        //4. Prossiga para o checkout.
         helper.waitForElement(SELECTORS.PROCEED_T0_CHECKOUT_BUTTON);
-
-        //4.          Prossiga para o checkout.
-        //Clicar no botão "Proceed to checkout"
         element(by.css(SELECTORS.PROCEED_T0_CHECKOUT_BUTTON)).click();
+
+        //5. Valide se o produto foi corretamente adicionado ao carrinho e prossiga caso esteja tudo certo.
         helper.waitForElement(SELECTORS.VALIDATION_AND_PROCEED_T0_CHECKOUT_BUTTON);
-
-        //5.          Valide se o produto foi corretamente adicionado ao carrinho e prossiga caso esteja tudo certo.
-        //Clicar no botão "Proceed to checkout"
         element(by.css(SELECTORS.VALIDATION_AND_PROCEED_T0_CHECKOUT_BUTTON)).click();
+
+        //6. Realize o cadastro do cliente preenchendo todos os campos obrigatórios dos formulários.
         helper.waitForElement(SELECTORS.RANDOM_EMAIL_FILL);
-
-        //6.          Realize o cadastro do cliente preenchendo todos os campos obrigatórios dos formulários.
-        //Preencher o campo "Email address" com o conteúdo "e-mail randômico" do lado esquerdo da tela
         element(by.css(SELECTORS.RANDOM_EMAIL_FILL)).sendKeys(USER_DATA.EMAIL_ADDRESS);
+
         helper.waitForElement(SELECTORS.CREATE_AN_ACCOUNT_BUTTON);
-
-        //Clicar no botão "Create an account"
         element(by.css(SELECTORS.CREATE_AN_ACCOUNT_BUTTON)).click();
-        helper.waitForElement(SELECTORS.MRS_BUTTON);
 
-        //Clicar no botão "Mrs."
+        helper.waitForElement(SELECTORS.MRS_BUTTON);
         element(by.css(SELECTORS.MRS_BUTTON)).click();
 
-        //Preencher o campo "First name" com o conteúdo "Fernanda"
         element(by.css(SELECTORS.FIRST_NAME_FILL)).sendKeys(USER_DATA.FIRST_NAME);
 
-        //Preencher o campo "Last name" com o conteúdo "Winter"
         element(by.css(SELECTORS.LAST_NAME_FILL)).sendKeys(USER_DATA.LAST_NAME);
 
-        //Clicar no campo "Email"
         element(by.css(SELECTORS.EMAIL_FILL)).click();
 
-        //Preencher o campo "Password" com o conteúdo "12345"
         element(by.css(SELECTORS.PASSWORD_FILL)).sendKeys(USER_DATA.PASSWORD);
 
-        //Preencher o campo "Address" com o conteúdo "305 May St"
         element(by.css(SELECTORS.ADDRESS_FILL)).sendKeys(USER_DATA.ADDRESS);
 
-        //Preencher o campo "City" com o conteúdo "Marshalltown"
         element(by.css(SELECTORS.CITY_FILL)).sendKeys(USER_DATA.CITY);
 
-        //Preencher o campo "State" com o conteúdo "Iowa"
         element(by.css(SELECTORS.STATE_FILL)).sendKeys(USER_DATA.STATE);
 
-        //Preencher o campo "Zip/Postal Code" com o conteúdo "50158"
         element(by.css(SELECTORS.POSTAL_CODE_FILL)).sendKeys(USER_DATA.POSTAL_CODE);
 
-        //Preencher o campo "Country" com o conteúdo "Unites States"
         element(by.css(SELECTORS.COUTRY_FILL)).sendKeys(USER_DATA.COUNTRY);
 
-        //Preencher o campo "Mobile phone" com o conteúdo "+1 651-772-3816"
         element(by.css(SELECTORS.MOBILE_PHONE_FILL)).sendKeys(USER_DATA.MOBILE_PHONE);
 
-        //Preencher o campo "Assign an address alias for future reference." com o conteúdo "My office"
         element(by.css(SELECTORS.ADDRESS_ALIAS_FILL)).clear();
         element(by.css(SELECTORS.ADDRESS_ALIAS_FILL)).sendKeys(USER_DATA.ADDRESS_ALIAS);
 
-        //Clicar no botão "Register"
         element(by.css(SELECTORS.REGISTER_BUTTON)).click();
 
-        //7.          Valide se o endereço está correto e prossiga.
-        //Clicar no botão "Proceed to checkout"
-        element(by.css(SELECTORS.ADDRESS_VALIDATION_AND_PROCEED_TO_CHECKOUT_BUTTON)).click();
+        //7. Valide se o endereço está correto e prossiga.
         helper.waitForElement(SELECTORS.CONFIRM_MY_ORDER_BUTTON);
+        element(by.css(SELECTORS.ADDRESS_VALIDATION_AND_PROCEED_TO_CHECKOUT_BUTTON)).click();
         (SELECTORS.TERMS_OF_SERVICE_CHECK), 10000;
 
-        //8.          Aceite os termos de serviço e prossiga.
-        //Clicar na caixa de seleção do "Terms of service"
-        element(by.css(SELECTORS.TERMS_OF_SERVICE_CHECK)).click();
+        //8. Aceite os termos de serviço e prossiga.
         helper.waitForElement(SELECTORS.CONFIRM_MY_ORDER_BUTTON);
+        element(by.css(SELECTORS.TERMS_OF_SERVICE_CHECK)).click();
         (SELECTORS.PROCEED_TO_THE_NEXT_STEP_BUTTON);
 
-        //Clicar no botão "Proceed to checkout"
         element(by.css(SELECTORS.PROCEED_TO_THE_NEXT_STEP_BUTTON)).click();
+
+        //9. Valide o valor total da compra.
+        //Verifica se o preço do produto está igual ao que estava na página no momento de selecionar para o carrinho de compra.
+
+        //10. Selecione um método de pagamento e prossiga.
         helper.waitForElement(SELECTORS.PAY_BY_BANK_WIRE_BUTTON);
-
-        //9.          Valide o valor total da compra.
-        //Verifica se o preço do produto está igual ao que estava na página no momento de selecionar para o carrinho de compra
-
-        //10.      Selecione um método de pagamento e prossiga.
-        //Clicar no botão "Pay by bank wire"
         element(by.css(SELECTORS.PAY_BY_BANK_WIRE_BUTTON)).click();
 
-        //11.      Confirme a compra e valide se foi finalizada com sucesso.
-        //Clicar no botão "I confirm my order"
+        //11. Confirme a compra e valide se foi finalizada com sucesso.
         helper.waitForElement(SELECTORS.CONFIRM_MY_ORDER_BUTTON);
         element(by.css(SELECTORS.CONFIRM_MY_ORDER_BUTTON)).click();
 
